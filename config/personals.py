6c42a9e5a915c9b40e554cbc93d48bcdfab869ec
Ariff Sayyad <125879041+ariffsayyad@users.noreply.github.com>
2026-09-17 22:12:40 +0530

    Update personals.py

--- config/personals.py
+++ config/personals.py
@@ -1,30 +1,15 @@
-'''
-Author:     Sai Vignesh Golla
-LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
-
-Copyright (c) 2024-2026 Sai Vignesh Golla
-
-License:    MIT License
-            https://opensource.org/license/mit
-            
-GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
-
-version:    2024.11.28.16.00
-'''
-
-
-###################################################### CONFIGURE YOUR TOOLS HERE ######################################################
+##################################################### CONFIGURE YOUR TOOLS HERE ######################################################
 
 
 # >>>>>>>>>>> Easy Apply Questions & Inputs <<<<<<<<<<<
 
 # Your legal name
-first_name = "Sai"                 # Your first name in quotes Eg: "First", "Sai"
-middle_name = "Vignesh"            # Your name in quotes Eg: "Middle", "Vignesh", ""
-last_name = "Golla"                # Your last name in quotes Eg: "Last", "Golla"
+first_name = "Ariff"                 # Your first name in quotes Eg: "First", "Sai"
+middle_name = " "            # Your name in quotes Eg: "Middle", "Vignesh", ""
+last_name = "Sayyad"                # Your last name in quotes Eg: "Last", "Golla"
 
 # Phone number (required), make sure it's valid.
-phone_number = "9876543210"        # Enter your 10 digit number in quotes Eg: "9876543210"
+phone_number = "000000000"        # Enter your 10 digit number in quotes Eg: "9876543210"
 
 # What is your current city?
 current_city = ""                  # Los Angeles, San Francisco, etc.
@@ -33,10 +18,10 @@ Note: If left empty as "", the bot will fill in location of jobs location.
 '''
 
 # Address, not so common question but some job applications make it required!
-street = "123 Main Street"
-state = "STATE"
-zipcode = "12345"
-country = "Will Let You Know When Established"
+street = "   "
+state = "   "
+zipcode = "   "
+country = "   "
 
 ## US Equal Opportunity questions
 # What is your ethnicity or race? If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
@@ -81,7 +66,7 @@ As an independent developer, I pour my heart and soul into creating tools like t
 Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
 
 Gratefully yours 🙏🏻,
-Sai Vignesh Golla
+Sayyad Ariff
 '''
 
 # --- Load user settings saved by the local control panel (user_config.json).
